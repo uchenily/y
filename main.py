960e12b1ad308c7bebb47ba0e994c48694f0b75d
uchenily <uchenily@qq.com>
2024-06-21 16:28:42 +0800

    Use argparse

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
 #!/usr/bin/env python3
+import argparse
+
 from interpreter import Interpreter
 from lexer import Lexer
 from parser import Parser
 from visualize_ast import VisualizeAST
 
-debug = False
-gen_ast = True
-
 
 def main():
-    program = None
-    import sys
-
-    file = "fibonacci.y"
-    if len(sys.argv) > 1:
-        file = sys.argv[1]
-    with open(file) as fp:
+    parser = argparse.ArgumentParser(prog="y-interpreter")
+    parser.add_argument("file", help="要执行的y脚本文件")
+    parser.add_argument("--debug", action="store_true", help="开启调试")
+    parser.add_argument("--ast", action="store_true", help="生成AST")
+    parser.add_argument("--ast-file", help="生成AST文件名", default="astree.dot")
+    args = parser.parse_args()
+    with open(args.file) as fp:
         program = fp.read()
 
     lexer = Lexer(program)
     tokens = lexer.run()
-    if debug:
+    if args.debug:
         print(tokens)
     parser = Parser(tokens)
     ast = parser.run()
-    if gen_ast:
+    if args.ast:
         ast_visitor = VisualizeAST()
         ast_visitor.visit(ast)
         dot = ast_visitor.gendot()
-        if debug:
+        if args.debug:
             print(dot)
-        with open("astree.dot", "w") as f:
+        with open(args.ast_file, "w") as f:
             f.write(dot)
 
     interpreter = Interpreter()
